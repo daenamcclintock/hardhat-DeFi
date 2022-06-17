71379e50f8a6d557e40d599810465af6dd9cac70
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-17 14:41:50 -0400

    initial commit

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,12 @@
+// Imports
+
+async function main() {
+    // Aave protocol treats everything as an ERC20 token
+}
+
+main()
+    .then(() => process.exit(0))
+    .then((error) => {
+        console.error(error)
+        process.exit(1)
+    })
